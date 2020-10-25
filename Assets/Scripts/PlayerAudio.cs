999be21d2933e2af51b60747f72b38e4f4fa39d5
hk547 <60270709+hk547@users.noreply.github.com>
2020-10-25 05:58:02 -0400

    Connecting to 3 Scenes (Error Fixed) Final Update

---
+++ Assets/Scripts/PlayerAudio.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlayerAudio : MonoBehaviour
+{
+    public AudioClip splashSound;
+
+    public AudioSource audioS;
+    public AudioMixerSnapshot idleSnapshot;
+    public AudioMixerSnapshot extraSnapshot;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("water"))
+        {
+            audioS.PlayOneShot(splashSound);
+        }
+        if (other.CompareTag("BossZone"))
+        {
+            extraSnapshot.TransitionTo(0.5f);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("water"))
+        {
+            audioS.PlayOneShot(splashSound);
+        }
+        if (other.CompareTag("BossZone"))
+        {
+            idleSnapshot.TransitionTo(0.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/SelectArea.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectArea : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Terrain"))
+        {
+            SceneManager.LoadScene("01-TerrainExperiment");
+        }else if (other.CompareTag("Interiors"))
+        {
+            SceneManager.LoadScene("02-Interiors");
+        }else if(other.CompareTag("Audio"))
+        {
+            SceneManager.LoadScene("03-Textures-Audio");
+        }
+        else
+        {
+            SceneManager.LoadScene("SceneManage");
+        }
+    }
+}
